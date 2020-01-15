0ad4dc1fda05598824ed08bc1d0e99df4bb60664
Danmer <danmerccoscco@gmail.com>
2020-01-15 00:42:20 -0500

    first commit

---
+++ src/ApiServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Danmercc\Apis;
+
+use Illuminate\Support\ServiceProvider;
+
+class ApiServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
